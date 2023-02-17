fd13ce254c4e4f1e1462a432441325848a1e59cd
Ian Clement <ian.clement@johnabbott.qc.ca>
2023-02-17 10:13:16 -0500

    Implementation of basic java class and comparable API.

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,3 +1,38 @@
-public class Pokemon {
+public class Pokemon implements Comparable<Pokemon> {
+    
+    public enum Type {
+        GRASS, FIRE, WATER
+    }
+
+    private String name;
+    private Type type;
+    private int level;
+
+    public Pokemon(String name, int level, Type type) {
+        this.name = name;
+        this.type = type;
+        this.level = level;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    @Override
+    public int compareTo(Pokemon rhs) {
+        int cmp = this.name.compareTo(rhs.name);
+        if (cmp == 0)
+            return this.level - rhs.level;
+        else
+            return cmp;
+    }
 
 }
